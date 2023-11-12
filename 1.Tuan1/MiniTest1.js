25ed9abb08951f3a1d7aca1f5ba4422a1f51d4d7
viethungle1 <hunglv0801@gmail.com>
2023-11-12 17:46:17 +0700

    minitest1

--- 1.Tuan1/MiniTest1.js
+++ 1.Tuan1/MiniTest1.js
@@ -1,12 +1,12 @@
 // Bài 1
 
-let number =parseInt(prompt("Mời bạn nhập một số bất kỳ"));
-let sqrt=number*number;
-if (number*1==number) {
-    alert("Bình phương của chữ số là " +sqrt);
-} else {
-    alert(" Mời bạn nhập lại ");
-}
+// let number =parseInt(prompt("Mời bạn nhập một số bất kỳ"));
+// let sqrt=number*number;
+// if (number*1==number) {
+//     alert("Bình phương của chữ số là " +sqrt);
+// } else {
+//     alert(" Mời bạn nhập lại ");
+// }
 
 // Bài 2
 
@@ -80,21 +80,21 @@ if (number*1==number) {
 // }
 
 // e)
-// function ketqua() {
-//     let a = document.getElementById("canha").value;
-//     let b = document.getElementById("canhb").value;
-//     let c = document.getElementById("canhc").value;
-//     if (a + b > c || b + c > a || a + c > b) {
-//         if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) {
-//             document.getElementById("ketqua").innerText = "Đây là tam giác vuông";
-//         } else if (a == b || b == c || a == c) {
-//             document.getElementById("ketqua").innerText = "Đây là tam giác cân"
-//         } else if (a == b && b == c && a == c) {
-//             document.getElementById("ketqua").innerText = "Đây là tam giác đều"
-//         } else {
-//             document.getElementById("ketqua").innerText = "Đây là hình tam gác"
-//         }
-//     } else {
-//         document.getElementById("ketqua").innerText = "Đây không phải là hình tam gác"
-//     }
-// }
+function ketqua() {
+    let a = document.getElementById("canha").value;
+    let b = document.getElementById("canhb").value;
+    let c = document.getElementById("canhc").value;
+    if (a + b > c || b + c > a || a + c > b) {
+        if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) {
+            document.getElementById("ketqua").innerText = "Đây là tam giác vuông";
+        } else if (a == b && b == c && a == c) {
+            document.getElementById("ketqua").innerText = "Đây là tam giác đều"
+        } else if (a == b || b == c || a == c) {
+            document.getElementById("ketqua").innerText = "Đây là tam giác cân"
+        } else {
+            document.getElementById("ketqua").innerText = "Đây là hình tam giác"
+        }
+    } else {
+        document.getElementById("ketqua").innerText = "Đây không phải là hình tam gác"
+    }
+}
