f547512196bd8ddf368ea287f7e6812e7b15a924
viethungle1 <hunglv0801@gmail.com>
2023-11-27 10:41:33 +0700

    minitest

--- 3.Tuan4/minitest3.js
+++ 3.Tuan4/minitest3.js
@@ -14,33 +14,33 @@
 
 // Bài 2:
 // Viết hàm in ra 15 số chia hết cho 3 đầu tiên bắt đầu từ số n.
-let start = +prompt("Nhập số bắt đầu")
-function print (x) {
-    let count = 0;
-    let result=""
-    while (count<15) {
-        if (x%3==0) {
-            result += x + '<br>'
-            count++
-        }
-        x++
-    }
-    return result
-}
-document.write(print(start))
+// let start = +prompt("Nhập số bắt đầu")
+// function print (x) {
+//     let count = 0;
+//     let result=""
+//     while (count<15) {
+//         if (x%3==0) {
+//             result += x + '<br>'
+//             count++
+//         }
+//         x++
+//     }
+//     return result
+// }
+// document.write(print(start))
 
 // B3:
 //     Cho 1 mảng số có sẵn.
 //     Viết hàm trả về 1 mảng các số >0 của mảng đã cho
 
-// let arr=[2,3,4,8,9,-5,1]
-// function positiveNumber (x) {
-//     let result = ""
-//     for (let i = 0; i < x.length; i++) {
-//         if (x[i]>0) {
-//             result += x[i]+'<br>'
-//         }
-//     }
-//     return result;
-// }
-// document.write(positiveNumber(arr))
+let arr=[2,3,4,8,9,-5,1]
+function newArr (x) {
+    let arr1=[]
+    for (let i = 0; i < x.length; i++) {
+        if (x[i]>0) {
+            arr1.push(x[i])
+        }
+    }
+    return arr1;
+}
+console.log(newArr(arr))
